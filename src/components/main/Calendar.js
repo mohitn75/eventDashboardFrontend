689c1052f5450250f2f1557249054dec30cacea2
mohitn75 <49280357+mohitn75@users.noreply.github.com>
2020-06-12 14:31:54 +0530

    Update Calendar.js

--- src/components/main/Calendar.js
+++ src/components/main/Calendar.js
@@ -1,41 +1,56 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { Route, withRouter } from 'react-router-dom';
+import axios from 'axios';
 import './material.css'
+import NavBar from './NavBar';
 
-function Calendar() {
+class CCalendar extends React.Component {
+  state = {
+    data : null
+}
+
+componentDidMount(){
+    this.assign();
+  }
+
+assign= () =>{
+
+  var config = {
+  method: 'get',
+  url: 'http://localhost:8080/api/eventsByUserId/1'
+  
+  };
+axios(config)
+.then( (response) => {
+  console.log(JSON.stringify(response.data));
+  console.log(response.data);
+  this.setState({data:response.data});
+  console.log(this.state.data);
+})
+.catch((error) =>{
+  console.log(error);
+});
+}
+
+  render(){
   return (
      <div >
     
   <div class="wrapper ">
-    <div class="sidebar" data-color="purple" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
- 
-      <div class="logo">
-          USER NAME
-        </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item ">
-            <a class="nav-link" href="/dash">
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="/calendar">
-              <p>Calendar</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="/alerts">
-              <p>Alerts</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <NavBar current='calendar' />
    <div class=" main-panel jumbotron">
-    calendar</div>
+    <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]}
+      events={this.state.data}
+      /*{[
+    { title: 'event 1', date: '2020-06-01T19:30' },
+    { title: 'event 2', date: '2020-06-02' }
+  ]}*/
+   /></div>
   </div>
 </div>   
 );
 }
-export default withRouter(Calendar);
+}
+export default withRouter(CCalendar);
